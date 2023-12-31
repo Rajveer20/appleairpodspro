6e5bc4ab8e6c379a5555ffe4578756012ef5a93b
Rajveer20 <72323161+Rajveer20@users.noreply.github.com>
2023-12-31 16:20:06 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-function getRandom(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
-}
+let arr = Array.from({length: 3001}, (_, i) => i + 2000);
 
-var arr = [4615, 4616, 4617, 4618, 4619, 4620, 4621, 4622, 4623, 4624, 4625, 4626, 4627, 4628, 4629, 4630, 4631, 4632, 4633, 4634, 4635, 4636, 4637, 4638, 4639, 4640, 4641, 4642, 4643, 4644, 4645, 4646, 4647, 4648, 4649, 4650, 4651, 4652, 4653, 4654, 4655, 4656, 4657, 4658, 4659, 4660, 4661, 4662, 4663, 4664, 4665, 4666, 4667, 4668, 4669, 4670, 4671, 4672, 4673, 4674, 4675, 4676, 4677, 4678, 4679, 4680, 4681, 4682, 4683, 4684, 4685, 4686, 4687, 4688, 4689, 4690, 4691, 4692, 4693, 4694, 4695, 4696, 4697, 4698, 4699, 4700, 4701, 4702, 4703, 4704, 4705, 4706, 4707, 4708, 4709, 4710, 4711, 4712, 4713, 4714, 4715, 4716, 4717, 4718, 4719, 4720];
 
 function shuffle(array) {
     let currentIndex = array.length,  randomIndex;
@@ -61,14 +56,14 @@ function showPrize(){
 
 function spin(){
     
-    for (var i = 0; i < 70; i++) { 
+    for (var i = 0; i < 90; i++) { 
         if(i<1){
-            setTimeout(confetti, 3600);
-            setTimeout(showPrize, 3600);
-            setTimeout(numberChangeFix, (70-i)*50);
+            setTimeout(confetti, 4600);
+            setTimeout(showPrize, 4600);
+            setTimeout(numberChangeFix, (90-i)*50);
         }
         else{
-            setTimeout(numberChange, (70-i)*50);
+            setTimeout(numberChange, (90-i)*50);
         }
     } 
 }
